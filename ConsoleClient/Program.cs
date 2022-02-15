2ab5109d5231a3e01925f54d35a417cc09776fa7
Savelii Kolesnikov <Savelii.Kolesnikov@covetrus.com>
2022-02-15 17:23:22 +0200

    Add project files.

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using VersOne.Epub;
+
+Console.WriteLine("Hello, World!");
+EpubBook epubBook = EpubReader.ReadBook("pelevin_iphuck-10_499568_fb2.epub");
+
+EpubContent bookContent = epubBook.Content;
+foreach (EpubTextContentFile htmlFile in bookContent.Html.Values)
+{
+    string htmlContent = htmlFile.Content;
+}
